4688ed982ec0a5468d5715474d2e5b0a15e45b20
jay <142081719+JayPornananratKan@users.noreply.github.com>
2024-12-08 15:13:24 +0700

    ปรับหน้า ui addproduct

--- src/app/admin/products/menu/page.tsx
+++ src/app/admin/products/menu/page.tsx
@@ -95,7 +95,7 @@ const MenuPage = () => {
         {createpopup && <AddProductForm />}
 
         {/* Product List */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
           {products.map((product) => (
             <div
               key={product.id}
