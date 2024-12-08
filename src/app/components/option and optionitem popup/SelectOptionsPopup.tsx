c84666be5d25e51ec81f950851a885bb139a3649
jay <142081719+JayPornananratKan@users.noreply.github.com>
2024-12-08 15:13:24 +0700

    ปรับหน้า ui addproduct

--- src/app/components/option and optionitem popup/SelectOptionsPopup.tsx
+++ src/app/components/option and optionitem popup/SelectOptionsPopup.tsx
@@ -26,14 +26,15 @@ const SelectOptionsPopup: React.FC<SelectOptionsPopupProps> = ({
   onCreateOption,
   onClose,
 }) => {
-    const [isCreateOptionPopupOpen, setIsCreateOptionPopupOpen] = useState(false);
-    const [selectedOptionForUpdate, setSelectedOptionForUpdate] = useState<OptionInterface | null>(null);
-    const handleUpdateOption = (option: OptionInterface) => {
-      setSelectedOptionForUpdate(option); // เปิด popup เพื่อแสดงรายการ option items
-    };
-    const handleCloseOptionItemPopup = () => {
-      setSelectedOptionForUpdate(null); // ปิด popup
-    };
+  const [isCreateOptionPopupOpen, setIsCreateOptionPopupOpen] = useState(false);
+  const [selectedOptionForUpdate, setSelectedOptionForUpdate] =
+    useState<OptionInterface | null>(null);
+  const handleUpdateOption = (option: OptionInterface) => {
+    setSelectedOptionForUpdate(option); // เปิด popup เพื่อแสดงรายการ option items
+  };
+  const handleCloseOptionItemPopup = () => {
+    setSelectedOptionForUpdate(null); // ปิด popup
+  };
   const handleOpenCreateOptionPopup = () => {
     setIsCreateOptionPopupOpen(true);
   };
@@ -42,7 +43,6 @@ const SelectOptionsPopup: React.FC<SelectOptionsPopupProps> = ({
     setIsCreateOptionPopupOpen(false);
   };
 
-  
   return (
     <div
       style={{
@@ -61,53 +61,64 @@ const SelectOptionsPopup: React.FC<SelectOptionsPopupProps> = ({
       <h2 style={{ textAlign: "center" }}>เลือกตัวเลือก</h2>
       {options.map((option) => (
         <div key={option.id}>
-          <input
-            type="checkbox"
-            checked={selectedOptions.some(
-              (selected) => selected.option.id === option.id
-            )}
-            onChange={() => onOptionChange(option)}
-          />
-          <label>{option.name}</label>
-          <button onClick={() => handleUpdateOption(option)}>อัปเดท</button>
+          <div className="flex justify-between items-center">
+            <div className="flex space-x-2 items-center">
+            <input
+            className="w-4 h-4"
+              type="checkbox"
+              checked={selectedOptions.some(
+                (selected) => selected.option.id === option.id
+              )}
+              onChange={() => onOptionChange(option)}
+              />
+            <label>{option.name}</label>
+              </div>
+            <button onClick={() => handleUpdateOption(option)}>อัปเดท</button>
+          </div>
+          <svg
+            className="w-full my-4"
+            height="1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="0"
+              y1="0"
+              x2="100%"
+              y2="0"
+              stroke="#D1D5DB"
+              strokeWidth="1"
+            />
+          </svg>
         </div>
       ))}
 
-      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "20px",
+        }}
+      >
         <button
           onClick={onClose}
-          style={{
-            padding: "10px 20px",
-            background: "#dc3545",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
+           className="text-black border border-black rounded-md px-4 py-2 hover:bg-black hover:text-white"
         >
           ปิด
         </button>
         {selectedOptionForUpdate && (
-        <OptionItemPopup
-        option={{
-          id: selectedOptionForUpdate.id!, // แปลงเป็น string แน่นอน
-          name: selectedOptionForUpdate.name,
-          require: selectedOptionForUpdate.require,
-        }}
-          onClose={handleCloseOptionItemPopup}
-        />
-      )}
+          <OptionItemPopup
+            option={{
+              id: selectedOptionForUpdate.id!, // แปลงเป็น string แน่นอน
+              name: selectedOptionForUpdate.name,
+              require: selectedOptionForUpdate.require,
+            }}
+            onClose={handleCloseOptionItemPopup}
+          />
+        )}
 
         <button
           onClick={handleOpenCreateOptionPopup}
-          style={{
-            padding: "10px 20px",
-            background: "#007bff",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
+          className="bg-black text-white border border-black rounded-md px-4 py-2 hover:bg-white hover:text-black"          
         >
           สร้างตัวเลือกใหม่
         </button>
