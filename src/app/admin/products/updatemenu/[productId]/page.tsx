bf678c412c7977a91793f8b9144ffd2cf70fa494
jay <142081719+JayPornananratKan@users.noreply.github.com>
2025-01-02 01:35:03 +0700

    ลบโหลดดิ่ง

--- src/app/admin/products/updatemenu/[productId]/page.tsx
+++ src/app/admin/products/updatemenu/[productId]/page.tsx
@@ -154,8 +154,6 @@ const UpdateProductForm = ({ productId,onClose }: { productId: string ,onClose: 
     }
   };
 
-  if (loading) return <p>Loading...</p>;
-
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg w-full max-w-3xl p-6">
